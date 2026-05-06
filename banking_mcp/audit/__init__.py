e729ff00baf78d12306d22ecd5c9f43f6ef8ce0f
DBoychew <dimitur.boychew@abv.bg>
2026-05-06 16:29:07 +0300

    Update banking_mcp/audit/__init__.py

--- banking_mcp/audit/__init__.py
+++ banking_mcp/audit/__init__.py
@@ -1,3 +1,4 @@
-from banking_mcp.audit.logger import log_request, log_error, start, stop
+from banking_mcp.audit.logger import log_error, log_query, start, stop
+from banking_mcp.audit.redaction import redact
 
-__all__ = ["log_request", "log_error", "start", "stop"]
+__all__ = ["log_error", "log_query", "start", "stop", "redact"]
