90f1af37dcda141916d59b7b36c66926b029e7d0
DBoychew <dimitur.boychew@abv.bg>
2026-05-06 16:29:10 +0300

    Update banking_mcp/db/__init__.py

--- banking_mcp/db/__init__.py
+++ banking_mcp/db/__init__.py
@@ -1,3 +1,18 @@
-from banking_mcp.db.manager import DatabaseManager, get_manager
+from banking_mcp.db.manager import DatabaseManager, LLMContext, SQL_DIALECT_HINTS, get_manager
+from banking_mcp.db.schema_fetcher import (
+    SchemaFetcher,
+    TableInfo,
+    get_schema_fetcher,
+    get_supported_db_types,
+)
 
-__all__ = ["DatabaseManager", "get_manager"]
+__all__ = [
+    "DatabaseManager",
+    "LLMContext",
+    "SQL_DIALECT_HINTS",
+    "get_manager",
+    "SchemaFetcher",
+    "TableInfo",
+    "get_schema_fetcher",
+    "get_supported_db_types",
+]
